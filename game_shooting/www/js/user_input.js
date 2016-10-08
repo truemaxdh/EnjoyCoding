f7ad248aec719ad6e1f6635bfd33899128039aa0
truemaxdh@gmail.com <truemaxdh@gmail.com>
2016-10-08 11:18:42 +0900

    test mobile

--- game_shooting/www/js/user_input.js
+++ game_shooting/www/js/user_input.js
@@ -3,13 +3,12 @@ var user_x, user_y;
 var user_pressing = false;
 
 function init_user_input() {
-    alert(canv_game.width);
     var scale_f = canv_game.width / canv_game.clientWidth;
     alert(scale_f);
     canv_game.onmousedown = function (e) {
-        user_x_ori = e.offsetX * scale_f;
+        user_x_ori = e.layerX * scale_f;
         user_y_ori = e.layerY * scale_f;
-        user_x = e.offsetX  * scale_f;
+        user_x = e.layerX  * scale_f;
         user_y = e.layerY * scale_f;
         user_pressing = true;
         return false;
@@ -21,15 +20,15 @@ function init_user_input() {
     }
 
     canv_game.onmousemove = function(e) {
-        user_x = e.offsetX * scale_f;
+        user_x = e.layerX * scale_f;
         user_y = e.layerY * scale_f;
         return false;
     }
 
     canv_game.ontouchstart = function (e) {
-        user_x_ori = e.touches[0].clientX * scale_f;
+        user_x_ori = e.touches[0].layerX * scale_f;
         user_y_ori = e.touches[0].clientY * scale_f;
-        user_x = e.touches[0].clientX * scale_f;
+        user_x = e.touches[0].layerX * scale_f;
         user_y = e.touches[0].clientY * scale_f;
         user_pressing = true;
         return false;
@@ -41,7 +40,7 @@ function init_user_input() {
     }
 
     canv_game.ontouchmove = function(e) {
-        user_x = e.touches[0].clientX * scale_f;
+        user_x = e.touches[0].layerX * scale_f;
         user_y = e.touches[0].clientY * scale_f;
         return false;
     }
