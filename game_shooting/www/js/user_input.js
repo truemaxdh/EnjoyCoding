8457dd69342212637a7179881f395a938d9995b2
truemaxdh@gmail.com <truemaxdh@gmail.com>
2016-10-08 09:29:04 +0900

    debug touch event

--- game_shooting/www/js/user_input.js
+++ game_shooting/www/js/user_input.js
@@ -23,11 +23,11 @@ function init_user_input() {
     }
 
     canv_game.ontouchstart = function (e) {
-        alert(e.screenX);
-        user_x_ori = e.screenX;
-        user_y_ori = e.screenY;
-        user_x = e.screenX;
-        user_y = e.screenY;
+        alert(e.touches[0].screenX);
+        user_x_ori = e.touches[0].screenX;
+        user_y_ori = e.touches[0].screenY;
+        user_x = e.touches[0].screenX;
+        user_y = e.touches[0].screenY;
         user_pressing = true;
         return false;
     }
@@ -38,8 +38,8 @@ function init_user_input() {
     }
 
     canv_game.ontouchmove = function(e) {
-        user_x = e.screenX;
-        user_y = e.screenY;
+        user_x = e.touches[0].screenX;
+        user_y = e.touches[0].screenY;
         return false;
     }
 }
