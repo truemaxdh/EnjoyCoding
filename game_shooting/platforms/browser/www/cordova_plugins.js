d68b0c17a9ea93fb12145cafc954c2c183b8ba0a
truemaxdh@gmail.com <truemaxdh@gmail.com>
2016-10-11 17:46:37 +0900

    develope game functions

--- game_shooting/platforms/browser/www/cordova_plugins.js
+++ game_shooting/platforms/browser/www/cordova_plugins.js
@@ -1,9 +1,20 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-admob-simple/www/AdMob.js",
+        "id": "cordova-plugin-admob-simple.AdMob",
+        "pluginId": "cordova-plugin-admob-simple",
+        "clobbers": [
+            "window.plugins.AdMob"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.0"
+    "cordova-plugin-whitelist": "1.3.0",
+    "cordova-admob-sdklibs": "2.1.4",
+    "cordova-plugin-admob-simple": "3.0.14"
 }
 // BOTTOM OF METADATA
 });
