9bcd18c1f0dd37ba3fd5a57577fd1ef396f04aa4
truemaxdh@gmail.com <truemaxdh@gmail.com>
2016-11-01 15:30:50 +0900

    edit style for menu

--- game_pentix/platforms/browser/platform_www/cordova_plugins.js
+++ game_pentix/platforms/browser/platform_www/cordova_plugins.js
@@ -1,14 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-game/www/game.js",
-        "id": "cordova-plugin-game.game",
-        "pluginId": "cordova-plugin-game",
-        "clobbers": [
-            "window.game"
-        ]
-    },
-    {
         "file": "plugins/cordova-connectivity-monitor/www/connectivity.js",
         "id": "cordova-connectivity-monitor.connectivity",
         "pluginId": "cordova-connectivity-monitor",
@@ -24,15 +16,24 @@ module.exports = [
             "window.admob",
             "window.tappx"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-game/www/game.js",
+        "id": "cordova-plugin-game.game",
+        "pluginId": "cordova-plugin-game",
+        "clobbers": [
+            "window.game"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.0",
-    "cordova-plugin-game": "1.0.120",
     "cordova-connectivity-monitor": "1.2.2",
-    "cordova-admob": "4.1.16"
+    "cordova-admob": "4.1.16",
+    "cordova-plugin-extras-google-play-services": "1.0.5",
+    "cordova-plugin-game": "1.0.120",
+    "cordova-plugin-whitelist": "1.3.0"
 }
 // BOTTOM OF METADATA
 });
---
+++ game_pentix/platforms/browser/www/js/pentix.js
@@ -0,0 +1,347 @@
+var COLS = 14, ROWS = 20;
+var BLOCK_WH = 5;
+var board = [];
+var score;
+var lose;
+var interval;
+var objInterval;
+var current; // current moving shape
+var currentX, currentY; // position of current shape
+var cleardLines;
+// var shapes = [
+//     [ 0, 0, 0, 0,
+//       1, 1, 1, 1 ],
+//     [ 0, 0, 0, 0,
+//       1, 1, 1, 0,
+//       1 ],
+//     [ 0, 0, 0, 0,
+//       1, 1, 1, 0,
+//       0, 0, 1 ],
+//     [ 0, 0, 0, 0,
+//       1, 1, 0, 0,
+//       1, 1 ],
+//     [ 0, 0, 0, 0,
+//       1, 1, 0, 0,
+//       0, 1, 1 ],
+//     [ 0, 0, 0, 0,
+//       0, 1, 1, 0,
+//       1, 1 ],
+//     [ 0, 0, 0, 0,
+//       0, 1, 0, 0,
+//       1, 1, 1 ]
+// ];
+
+var shapes = [
+    // 5
+    [ 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0,
+      1, 1, 1, 1, 1 ],
+    // 4
+    [ 0, 0, 0, 0, 0,
+      1, 1, 1, 1, 0,
+      1 ],
+    [ 0, 0, 0, 0, 0,
+      1, 1, 1, 1, 0,
+      0, 1 ],
+    [ 0, 0, 0, 0, 0,
+      1, 1, 1, 1, 0,
+      0, 0, 1 ],
+    [ 0, 0, 0, 0, 0,
+      1, 1, 1, 1, 0,
+      0, 0, 0, 1 ],
+    // 3
+    [ 0, 0, 0, 0, 0,
+      0, 1, 1, 1, 0,
+      1, 1 ],
+    [ 0, 0, 0, 0, 0,
+      0, 1, 1, 1, 0,
+      0, 1, 1 ],
+    [ 0, 0, 0, 0, 0,
+      0, 1, 1, 1, 0,
+      0, 0, 1, 1 ],
+    [ 0, 0, 0, 0, 0,
+      0, 1, 1, 1, 0,
+      0, 0, 0, 1, 1 ],
+    [ 0, 0, 0, 0, 0,
+      0, 1, 1, 1, 0,
+      0, 1, 0, 0, 0,
+      0, 1 ],
+    [ 0, 0, 0, 0, 0,
+      0, 1, 1, 1, 0,
+      0, 0, 1, 0, 0,
+      0, 0, 1 ],
+    [ 0, 0, 0, 0, 0,
+      0, 1, 1, 1, 0,
+      0, 0, 0, 1, 0, 
+      0, 0, 0, 1 ],
+    // 2
+    [ 0, 0, 0, 0, 0,
+      0, 1, 1, 0, 0,
+      0, 1, 0, 0, 0,
+      0, 1, 1 ],
+    [ 0, 0, 0, 0, 0,
+      0, 0, 1, 1, 0,
+      0, 1, 1, 0, 0,
+      0, 1 ],
+    [ 0, 0, 0, 0, 0,
+      0, 0, 1, 1, 0,
+      0, 1, 1, 0, 0,
+      0, 0, 1 ],
+    [ 0, 0, 0, 0, 0,
+      0, 1, 1, 0, 0,
+      0, 0, 1, 1, 0,
+      0, 0, 1 ],
+    [ 0, 0, 0, 0, 0,
+      0, 1, 1, 0, 0,
+      0, 0, 1, 0, 0,
+      0, 0, 1, 1 ],
+    [ 0, 0, 0, 0, 0,
+      0, 0, 1, 1, 0,
+      0, 0, 1, 0, 0,
+      0, 1, 1 ],
+    [ 0, 0, 0, 0, 0,
+      0, 0, 1, 0, 0,
+      0, 1, 1, 1, 0,
+      0, 0, 1 ]
+];
+
+// creates a new 5x5 shape in global variable 'current'
+// 5x5 so as to cover the size when the shape is rotated
+function newShape() {
+    var id = Math.floor( Math.random() * shapes.length );
+    var shape = shapes[ id ]; // maintain id for color filling
+
+    current = [];
+    for ( var y = 0; y < BLOCK_WH; ++y ) {
+        current[ y ] = [];
+        for ( var x = 0; x < BLOCK_WH; ++x ) {
+            var i = BLOCK_WH * y + x;
+            if ( typeof shape[ i ] != 'undefined' && shape[ i ] ) {
+                current[ y ][ x ] = id + 1;
+            }
+            else {
+                current[ y ][ x ] = 0;
+            }
+        }
+    }
+    // position where the shape will evolve
+    currentX = 5;
+    currentY = -1;
+}
+
+// clears the board
+function init() {
+    for ( var y = 0; y < ROWS; ++y ) {
+        board[ y ] = [];
+        for ( var x = 0; x < COLS; ++x ) {
+            board[ y ][ x ] = 0;
+        }
+    }
+}
+
+// keep the element moving down, creating new shapes and clearing lines
+function tick() {
+    if ( valid( 0, 1 ) ) {
+        ++currentY;
+    }
+    // if the element settled
+    else {
+        freeze();
+        clearLines();
+        if (lose) {
+            gameOver();
+            return false;
+        }
+        newShape();
+    }
+}
+
+function gameOver() {
+    clearInterval(objInterval);
+    if (isApp) {
+        window.game.submitScore(leaderboardId, score);
+        window.game.onSubmitScoreSucceeded = function() {
+            OpenUserResult();
+        };
+        window.game.onSubmitScoreFailed = function() {
+            OpenUserResult();
+        };
+    } else {
+        OpenUserResult();
+    }
+    document.getElementById('user_score').innerHTML = score;
+
+}
+
+// stop shape at its position and fix it to board
+function freeze() {
+    for ( var y = 0; y < BLOCK_WH; ++y ) {
+        for ( var x = 0; x < BLOCK_WH; ++x ) {
+            if ( current[ y ][ x ] ) {
+                board[ y + currentY ][ x + currentX ] = current[ y ][ x ];
+            }
+        }
+    }
+}
+
+// returns rotates the rotated shape 'current' perpendicularly anticlockwise
+function rotate( current ) {
+    var newCurrent = [];
+    for ( var y = 0; y < BLOCK_WH; ++y ) {
+        newCurrent[ y ] = [];
+        for ( var x = 0; x < BLOCK_WH; ++x ) {
+            newCurrent[ y ][ x ] = current[ BLOCK_WH - 1 - x ][ y ];
+        }
+    }
+
+    return newCurrent;
+}
+
+// check if any lines are filled and clear them
+function clearLines() {
+    var combo = 0;
+    for ( var y = ROWS - 1; y >= 0; --y ) {
+        var rowFilled = true;
+        //var unfilledCnt = 0;
+        for ( var x = 0; x < COLS; ++x ) {
+            if ( board[ y ][ x ] == 0 ) {
+                //unfilledCnt++;
+                //if (unfilledCnt > 2) {
+                    rowFilled = false;
+                    break;
+                //}
+            } 
+        }
+        if ( rowFilled ) {
+            cleardLines++;
+            chkAndUnlockAchievement(cleardLines);
+            score += ++combo * 10; 
+            document.getElementById( 'score_num' ).innerHTML = score;
+            document.getElementById( 'clearsound' ).play();
+            for ( var yy = y; yy > 0; --yy ) {
+                for ( var x = 0; x < COLS; ++x ) {
+                    board[ yy ][ x ] = board[ yy - 1 ][ x ];
+                }
+            }
+            //clearInterval(objInterval);
+            //interval -= 10;
+            //objInterval = setInterval( tick, interval );
+            ++y;
+        }
+    }
+}
+
+var rotBlTmr = 0;
+function procKeyEvent() {
+    switch ( keyCode ) {
+        case 'left':
+            if ( valid( -1 ) ) {
+                --currentX;
+            }
+            break;
+        case 'right':
+            if ( valid( 1 ) ) {
+                ++currentX;
+            }
+            break;
+        case 'down':
+            if ( valid( 0, 1 ) ) {
+                ++currentY;
+            }
+            break;
+        case 'rotate':
+            if (rotBlTmr--==0) {
+                var rotated = rotate( current );
+                if ( valid( 0, 0, rotated ) ) {
+                    current = rotated;
+                }
+                rotBlTmr = 1;
+            }
+            break;
+    }
+}
+
+function procTouchEvent() {
+    keyCode = '';
+    if (user_pressing) {
+        var dx = user_x - user_x_ori;
+        var dy = user_y - user_y_ori;
+        if (dx < Math.abs(BLOCK_W / 2) && dy < Math.abs(BLOCK_H / 2)) {
+            return;
+        }
+
+        do_rotate = false;
+        if (dx > BLOCK_W) {
+            keyCode = 'right';
+            user_x_ori = user_x_ori + BLOCK_W;
+        } else if (dx < - BLOCK_W) {
+            keyCode = 'left';
+            user_x_ori = user_x_ori - BLOCK_W;
+        }
+        if (dy > BLOCK_H) {
+            keyCode = 'down';
+            user_y_ori = user_y_ori + BLOCK_H;
+        }
+    } else {
+        if (do_rotate) {
+            rotBlTmr = 0;
+            keyCode = 'rotate';
+            do_rotate = false;
+        }
+    }
+}
+
+// checks if the resulting position of current shape will be feasible
+function valid( offsetX, offsetY, newCurrent ) {
+    offsetX = offsetX || 0;
+    offsetY = offsetY || 0;
+    offsetX = currentX + offsetX;
+    offsetY = currentY + offsetY;
+    newCurrent = newCurrent || current;
+
+
+
+    // for ( var y = 0; y < 4; ++y ) {
+    for ( var y = 0; y < BLOCK_WH; ++y ) {
+        // for ( var x = 0; x < 4; ++x ) {
+        for ( var x = 0; x < BLOCK_WH; ++x ) {
+            if ( newCurrent[ y ][ x ] ) {
+                if ( typeof board[ y + offsetY ] == 'undefined'
+                  || typeof board[ y + offsetY ][ x + offsetX ] == 'undefined'
+                  || board[ y + offsetY ][ x + offsetX ]
+                  || x + offsetX < 0
+                  || y + offsetY >= ROWS
+                  || x + offsetX >= COLS ) {
+                    // if (offsetY == 1) lose = true; // lose if the current shape at the top row when checked
+                    if (offsetY < 1) lose = true; // lose if the current shape at the top row when checked
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+function newGame() {
+    clearInterval(objInterval);
+    init();
+    score = 0;
+    cleardLines = 0;
+    newShape();
+    lose = false;
+    interval = 500;
+    objInterval = setInterval( tick, interval );
+    
+    //render_init();
+    clearInterval(render_interval);
+    render_interval = setInterval( render, 100 );
+}
+
+function togglePause() {
+    if (objInterval > 0) {
+        clearInterval(objInterval);
+        objInterval = -1;
+    } else {
+        objInterval = setInterval( tick, interval );
+    }
+}
