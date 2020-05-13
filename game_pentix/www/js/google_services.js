98120d6dc067c2b2a502c4dbb216d54a042b6945
truemaxdh <truemaxdh@gmail.com>
2020-05-13 16:40:45 +0900

    Update google_services.js

--- game_pentix/www/js/google_services.js
+++ game_pentix/www/js/google_services.js
@@ -1,38 +1,38 @@
 // admob
 //initialize the goodies 
 function initAd(){
-        /*if ( window.plugins && window.plugins.AdMob ) {
-            var ad_units = {
-                ios : {
-                    banner: 'ca-app-pub-xxxxxxxxxxx/xxxxxxxxxxx',		//PUT ADMOB ADCODE HERE 
-                    interstitial: 'ca-app-pub-xxxxxxxxxxx/xxxxxxxxxxx'	//PUT ADMOB ADCODE HERE 
-                },
-                android : {
-                    banner: 'ca-app-pub-7307479428475282/6915509453',		//PUT ADMOB ADCODE HERE 
-                    interstitial: 'ca-app-pub-7307479428475282/5184863454'	//PUT ADMOB ADCODE HERE 
-                }
-            };
-            var admobid = ( /(android)/i.test(navigator.userAgent) ) ? ad_units.android : ad_units.ios;
- 
-            window.plugins.AdMob.setOptions( {
-                publisherId: admobid.banner,
-                interstitialAdId: admobid.interstitial,
-                adSize: window.plugins.AdMob.AD_SIZE.SMART_BANNER,	//use SMART_BANNER, BANNER, IAB_MRECT, IAB_BANNER, IAB_LEADERBOARD 
-                bannerAtTop: false, // set to true, to put banner at top 
-                overlap: true, // banner will overlap webview  
-                offsetTopBar: false, // set to true to avoid ios7 status bar overlap 
-                isTesting: false, // receiving test ad 
-                autoShow: false // auto show interstitial ad when loaded 
-            });
- 
-            registerAdEvents();
-            window.plugins.AdMob.createInterstitialView();	//get the interstitials ready to be shown 
-            window.plugins.AdMob.requestInterstitialAd();
- 
-            showHideBanner(true);
-        } else {
-            //alert( 'admob plugin not ready' ); 
-        }*/
+  /*if ( window.plugins && window.plugins.AdMob ) {
+      var ad_units = {
+          ios : {
+              banner: 'ca-app-pub-xxxxxxxxxxx/xxxxxxxxxxx',		//PUT ADMOB ADCODE HERE 
+              interstitial: 'ca-app-pub-xxxxxxxxxxx/xxxxxxxxxxx'	//PUT ADMOB ADCODE HERE 
+          },
+          android : {
+              banner: 'ca-app-pub-7307479428475282/6915509453',		//PUT ADMOB ADCODE HERE 
+              interstitial: 'ca-app-pub-7307479428475282/5184863454'	//PUT ADMOB ADCODE HERE 
+          }
+      };
+      var admobid = ( /(android)/i.test(navigator.userAgent) ) ? ad_units.android : ad_units.ios;
+
+      window.plugins.AdMob.setOptions( {
+          publisherId: admobid.banner,
+          interstitialAdId: admobid.interstitial,
+          adSize: window.plugins.AdMob.AD_SIZE.SMART_BANNER,	//use SMART_BANNER, BANNER, IAB_MRECT, IAB_BANNER, IAB_LEADERBOARD 
+          bannerAtTop: false, // set to true, to put banner at top 
+          overlap: true, // banner will overlap webview  
+          offsetTopBar: false, // set to true to avoid ios7 status bar overlap 
+          isTesting: false, // receiving test ad 
+          autoShow: false // auto show interstitial ad when loaded 
+      });
+
+      registerAdEvents();
+      window.plugins.AdMob.createInterstitialView();	//get the interstitials ready to be shown 
+      window.plugins.AdMob.requestInterstitialAd();
+
+      showHideBanner(true);
+  } else {
+      //alert( 'admob plugin not ready' ); 
+  }*/
   try {
     Android.adMobInit();
     Android.adMobInterstitialLoad();
@@ -44,35 +44,35 @@ function initAd(){
 
 /*functions to allow you to know when ads are shown, etc. 
 function registerAdEvents() {
-        document.addEventListener('onReceiveAd', function(){});
-        document.addEventListener('onFailedToReceiveAd', function(data){});
-        document.addEventListener('onPresentAd', function(){});
-        document.addEventListener('onDismissAd', function(){ });
-        document.addEventListener('onLeaveToAd', function(){ });
-        document.addEventListener('onReceiveInterstitialAd', function(){ });
-        document.addEventListener('onPresentInterstitialAd', function(){ });
-        document.addEventListener('onDismissInterstitialAd', function(){
-        	//window.plugins.AdMob.createInterstitialView();			//REMOVE THESE 2 LINES IF USING AUTOSHOW 
-            //window.plugins.AdMob.requestInterstitialAd();			//get the next one ready only after the current one is closed 
-        });
-    }
+    document.addEventListener('onReceiveAd', function(){});
+    document.addEventListener('onFailedToReceiveAd', function(data){});
+    document.addEventListener('onPresentAd', function(){});
+    document.addEventListener('onDismissAd', function(){ });
+    document.addEventListener('onLeaveToAd', function(){ });
+    document.addEventListener('onReceiveInterstitialAd', function(){ });
+    document.addEventListener('onPresentInterstitialAd', function(){ });
+    document.addEventListener('onDismissInterstitialAd', function(){
+      //window.plugins.AdMob.createInterstitialView();			//REMOVE THESE 2 LINES IF USING AUTOSHOW 
+        //window.plugins.AdMob.requestInterstitialAd();			//get the next one ready only after the current one is closed 
+    });
+}
 */
 /*
 //display the banner 
 function showHideBanner(bShow) {
-    if (isApp) {
-        if (bShow) {
-            window.plugins.AdMob.createBannerView();
-        } else {
-            window.plugins.AdMob.destroyBannerView();
-        } 
-    }
+  if (isApp) {
+    if (bShow) {
+      window.plugins.AdMob.createBannerView();
+    } else {
+      window.plugins.AdMob.destroyBannerView();
+    } 
+  }
 }
 
 
 //display the interstitial 
 function showInterstitialFunc(){
-    window.plugins.AdMob.showInterstitialAd();
+  window.plugins.AdMob.showInterstitialAd();
 }
 */
 
@@ -87,14 +87,14 @@ var achvIds = ["CgkItYKH-eAXEAIQAQ",
 var achvLines = [1,3,6,10,15];
 
 function chkAndUnlockAchievement(lineCnt) {
-    var idx = achvLines.indexOf(lineCnt);
-    if (idx > -1) {
-      try {  
-        Android.unlockAchievement(achvIds[idx]);
-      } catch(e) {
-        toast("unlockAchievement failed.");
-      }
+  var idx = achvLines.indexOf(lineCnt);
+  if (idx > -1) {
+    try {  
+      Android.unlockAchievement(achvIds[idx]);
+    } catch(e) {
+      toast("unlockAchievement failed.");
     }
+  }
 }
 /*
 function GetUserImg() {
@@ -108,49 +108,49 @@ function GetUserImg() {
 }
 */
 function OpenUserResult() {
-    document.getElementById("user_result").style.width = "100%";
+  document.getElementById("user_result").style.width = "100%";
 }
 
 function  CloseUserResult() {
-    document.getElementById("user_result").style.width = "0%";
-    pageChange('menu');
+  document.getElementById("user_result").style.width = "0%";
+  pageChange('menu');
 }
 
 function ShowAchievements() {
-    try {
-	    Android.showAchievements();
-    } catch(e) {
-      toast("failed.");
-    }
+  try {
+    Android.showAchievements();
+  } catch(e) {
+    toast("failed.");
+  }
 }
 
 function ShowHighScores() {
-    try {
-        Android.showLeaderboard();
-    } catch(e) {
-      toast("failed.");
-    }
+  try {
+      Android.showLeaderboard();
+  } catch(e) {
+    toast("failed.");
+  }
 }
 
 // pages
 var pageIDs = ['intro','menu','game'];
 function pageChange(newpageID) {
-    for (var i = 0; i < pageIDs.length; i++) {
-        var page = document.getElementById(pageIDs[i]);
-        if (pageIDs[i]==newpageID) {
-            page.style.display = 'block';
-        } else {
-            page.style.display = 'none';
-        }
+  for (var i = 0; i < pageIDs.length; i++) {
+    var page = document.getElementById(pageIDs[i]);
+    if (pageIDs[i]==newpageID) {
+      page.style.display = 'block';
+    } else {
+      page.style.display = 'none';
     }
+  }
 
-    // if (newpageID=='menu') {
-    //     //removeEvt();
-    // } else 
-    if (newpageID=='game') {
-        addEvt();
-        newGame();
-    } 
+  // if (newpageID=='menu') {
+  //     //removeEvt();
+  // } else 
+  if (newpageID=='game') {
+    addEvt();
+    newGame();
+  } 
 }
 
 function toast(msg) {
@@ -186,6 +186,7 @@ function onDeviceReady() {
   try {
     Android.signInToGS();
   } catch(e) {
+    toast("signInToGS failed.");
   }
   render_init();
 }
