a465af5d38f38d6de5c2c0beaf0091bb6a060983
truemaxdh <truemaxdh@gmail.com>
2020-06-09 11:50:11 +0900

    Update google_api_js.js

--- game_shooting/www/js/google_api_js.js
+++ game_shooting/www/js/google_api_js.js
@@ -1,90 +1,85 @@
-// admob
-//initialize the goodies 
-function initAd(){
-        if ( window.plugins && window.plugins.AdMob ) {
-            var ad_units = {
-                ios : {
-                    banner: 'ca-app-pub-xxxxxxxxxxx/xxxxxxxxxxx',		//PUT ADMOB ADCODE HERE 
-                    interstitial: 'ca-app-pub-xxxxxxxxxxx/xxxxxxxxxxx'	//PUT ADMOB ADCODE HERE 
-                },
-                android : {
-                    banner: 'ca-app-pub-7307479428475282/2327029858',		//PUT ADMOB ADCODE HERE 
-                    interstitial: 'ca-app-pub-7307479428475282/5329307450'	//PUT ADMOB ADCODE HERE 
-                }
-            };
-            var admobid = ( /(android)/i.test(navigator.userAgent) ) ? ad_units.android : ad_units.ios;
- 
-            window.plugins.AdMob.setOptions( {
-                publisherId: admobid.banner,
-                interstitialAdId: admobid.interstitial,
-                adSize: window.plugins.AdMob.AD_SIZE.SMART_BANNER,	//use SMART_BANNER, BANNER, IAB_MRECT, IAB_BANNER, IAB_LEADERBOARD 
-                bannerAtTop: false, // set to true, to put banner at top 
-                overlap: true, // banner will overlap webview  
-                offsetTopBar: false, // set to true to avoid ios7 status bar overlap 
-                isTesting: false, // receiving test ad 
-                autoShow: false // auto show interstitial ad when loaded 
-            });
- 
-            registerAdEvents();
-            window.plugins.AdMob.createInterstitialView();	//get the interstitials ready to be shown 
-            window.plugins.AdMob.requestInterstitialAd();
- 
-            showHideBanner(true);
-        } else {
-            //alert( 'admob plugin not ready' ); 
-        }
-}
-//functions to allow you to know when ads are shown, etc. 
-function registerAdEvents() {
-        document.addEventListener('onReceiveAd', function(){});
-        document.addEventListener('onFailedToReceiveAd', function(data){});
-        document.addEventListener('onPresentAd', function(){});
-        document.addEventListener('onDismissAd', function(){ });
-        document.addEventListener('onLeaveToAd', function(){ });
-        document.addEventListener('onReceiveInterstitialAd', function(){ });
-        document.addEventListener('onPresentInterstitialAd', function(){ });
-        document.addEventListener('onDismissInterstitialAd', function(){
-        	//window.plugins.AdMob.createInterstitialView();			//REMOVE THESE 2 LINES IF USING AUTOSHOW 
-            //window.plugins.AdMob.requestInterstitialAd();			//get the next one ready only after the current one is closed 
-        });
+// AdMob
+var AdMob = {
+  adStatus : 0,
+  onInitComplete : function() {
+    AdMob.adStatus = 1; 
+    try {
+      Android.adMobInitInterstitial("ca-app-pub-3940256099942544/1033173712");
+    } catch(e) {
+      toast("adMobInitInterstitial failed.");
     }
-
+    /*try {
+      Android.adMobInitBanner(); 
+    } catch(e) {
+      toast("adMobInitBanner failed.");
+    }*/
+    /*try {
+      Android.adMobIntertitialSetToUseJSCallback();
+    } catch(e) {
+      toast("adMobInterstitialSetToUseJSCallback failed.");
+    }*/
+  },
+  Interstitial : {
+    clsName : "AdMob.Interstitial",
+    onAdLoaded : function() {
+      AdMob.adStatus = 2;
+      toast("adMob Intertitial Load Complete.");
+    },
+    onAdFailedToLoad : function() {
+      toast("adMob Intertitial Load Failed.");
+    },
+    onAdOpened : function() {
+      toast("adMob Intertitial Ad Opened.");
+    },
+    onAdClicked : function() {
+      toast("adMob Intertitial Ad Clicked.");
+    },
+    onAdLeftApplication : function() {
+      toast("adMob Intertitial Ad Left Application.");
+    },
+    onAdClosed : function() {
+      toast("adMob Intertitial Ad Closed.");
+    }
+  }
+}
+/*
 //display the banner 
 function showHideBanner(bShow) {
-    if (isApp) {
-        if (bShow) {
-            window.plugins.AdMob.createBannerView();
-        } else {
-            window.plugins.AdMob.destroyBannerView();
-        } 
-    }
+  if (isApp) {
+    if (bShow) {
+      window.plugins.AdMob.createBannerView();
+    } else {
+      window.plugins.AdMob.destroyBannerView();
+    } 
+  }
 }
-
-//display the interstitial 
-function showInterstitialFunc(){
-    window.plugins.AdMob.showInterstitialAd();
-}
-
-
-
+*/
 
 // Google Game Service Ids
 var leaderboardId = "CgkIwPzgz_EBEAIQBg";
-var achvIds = ["CgkIwPzgz_EBEAIQAQ",
-               "CgkIwPzgz_EBEAIQAg",
-               "CgkIwPzgz_EBEAIQAw",
-               "CgkIwPzgz_EBEAIQBA",
-               "CgkIwPzgz_EBEAIQBQ"];
+var achvIds = [
+  "CgkIwPzgz_EBEAIQAQ",
+  "CgkIwPzgz_EBEAIQAg",
+  "CgkIwPzgz_EBEAIQAw",
+  "CgkIwPzgz_EBEAIQBA",
+  "CgkIwPzgz_EBEAIQBQ"
+];
 
 var achvScores = [500, 2000, 5000, 10000, 20000];
-
+var glGameSvc = {
+  loginStatus : false
+};
 function chkAndUnlockAchievement(score) {
-    var idx = achvScores.indexOf(score);
-    if (idx > -1) {
-        window.game.unlockAchievement(achvIds[idx]);
+  var idx = achvScores.indexOf(score);
+  if (idx > -1) {
+    try {  
+      Android.unlockAchievement(achvIds[idx]);
+    } catch(e) {
+      toast("unlockAchievement failed.");
     }
+  }
 }
-
+/*
 function GetUserImg() {
     window.game.getPlayerImage();
     window.game.onGetPlayerImageSucceeded = function(result) {
@@ -94,20 +89,28 @@ function GetUserImg() {
         
     };	
 }
-
+*/
 function OpenUserResult() {
-    document.getElementById("user_result").style.width = "100%";
+  document.getElementById("user_result").style.width = "100%";
 }
 
 function  CloseUserResult() {
-    document.getElementById("user_result").style.width = "0%";
-    pageChange('menu');
+  document.getElementById("user_result").style.width = "0%";
+  pageChange('menu');
 }
 
 function ShowAchievements() {
-    window.game.showAchievements();
+  try {
+    Android.showAchievements();
+  } catch(e) {
+    toast("failed.");
+  }
 }
 
 function ShowHighScores() {
-    window.game.showLeaderboard(leaderboardId);
+  try {
+    Android.showLeaderboard(leaderboardId);
+  } catch(e) {
+    toast("failed.");
+  }
 }
