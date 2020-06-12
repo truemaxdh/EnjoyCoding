92b1075017b0b9bdeddbd8cfbe79a3b0633c2761
truemaxdh <truemaxdh@gmail.com>
2020-06-12 15:20:12 +0900

    Update user_input.js

--- game_shooting/www/js/user_input.js
+++ game_shooting/www/js/user_input.js
@@ -3,57 +3,57 @@ var user_x, user_y;
 var user_pressing = false;
 
 function init_user_input() {
-    var scale_f = canv_game.width / canv_game.clientWidth;
+  var scale_f = canv_game.width / canv_game.clientWidth;
+  
+  canv_game.onmousedown = function (e) {
+    user_x = e.clientX  * scale_f;
+    user_y = e.clientY * scale_f;
+    user_x_ori = user_x;
+    user_y_ori = user_y;
     
-    canv_game.onmousedown = function (e) {
-        user_x = e.clientX  * scale_f;
-        user_y = e.clientY * scale_f;
-        user_x_ori = user_x;
-        user_y_ori = user_y;
-        
-        if (user_y_ori <= 60 && user_x_ori > 400) {
-            showSubMenu();
-        }
-        
-        user_pressing = true;
-        return false;
+    if (user_y_ori <= 60 && user_x_ori > 400) {
+      showSubMenu();
     }
+    
+    user_pressing = true;
+    return false;
+  }
 
-    canv_game.onmouseup = function (e) {
-        user_pressing = false;
-        return false;
-    }
+  canv_game.onmouseup = function (e) {
+    user_pressing = false;
+    return false;
+  }
 
-    canv_game.onmousemove = function(e) {
-        user_x = e.layerX * scale_f;
-        user_y = e.layerY * scale_f;
-        return false;
-    }
+  canv_game.onmousemove = function(e) {
+    user_x = e.layerX * scale_f;
+    user_y = e.layerY * scale_f;
+    return false;
+  }
 
-    canv_game.ontouchstart = function (e) {
-        user_x = e.touches[0].clientX * scale_f;
-        user_y = e.touches[0].clientY * scale_f;
-        user_x_ori = user_x;
-        user_y_ori = user_y;
-        
-        if (user_y_ori <= 60 && user_x_ori > 400) {
-            showSubMenu();
-        }
-        
-        user_pressing = true;
-        return false;
+  canv_game.ontouchstart = function (e) {
+    user_x = e.touches[0].clientX * scale_f;
+    user_y = e.touches[0].clientY * scale_f;
+    user_x_ori = user_x;
+    user_y_ori = user_y;
+    
+    if (user_y_ori <= 60 && user_x_ori > 400) {
+      showSubMenu();
     }
+    
+    user_pressing = true;
+    return false;
+  }
 
-    canv_game.ontouchend = function (e) {
-        user_pressing = false;
-        return false;
-    }
+  canv_game.ontouchend = function (e) {
+    user_pressing = false;
+    return false;
+  }
 
-    canv_game.ontouchmove = function(e) {
-        user_x = e.touches[0].clientX * scale_f;
-        user_y = e.touches[0].clientY * scale_f;
-        return false;
-    }
+  canv_game.ontouchmove = function(e) {
+    user_x = e.touches[0].clientX * scale_f;
+    user_y = e.touches[0].clientY * scale_f;
+    return false;
+  }
 }
 
 function showSubMenu() {
@@ -69,17 +69,18 @@ function hideSubMenu() {
   paused = false;
   if (curPage == 'game') {
     document.getElementById( 'bgm' ).play();
+    last_animation_time = 0;
     tick();
   }
 }
 
 /*function togglePause() {
-    pause = !pause;
-    if (!pause) {
-        document.getElementById( 'bgm' ).play();
-        last_animation_time = 0;
-        requestAnimationFrame(tick);
-    } else {
-        document.getElementById( 'bgm' ).pause();
-    }
+  pause = !pause;
+  if (!pause) {
+    document.getElementById( 'bgm' ).play();
+    last_animation_time = 0;
+    requestAnimationFrame(tick);
+  } else {
+    document.getElementById( 'bgm' ).pause();
+  }
 }*/
