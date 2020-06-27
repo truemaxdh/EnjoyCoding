9c863b978ce7cb42041d413ef4e3de5dec223c25
truemaxdh <truemaxdh@gmail.com>
2020-06-27 17:34:50 +0900

    Create user_input.js

---
+++ game_dokebi_ball/www/js/user_input.js
@@ -0,0 +1,75 @@
+var user_x_ori, user_y_ori;
+var user_x, user_y;
+var user_pressing = false;
+
+function init_user_input() {
+  var scale_f = canv_game.width / canv_game.clientWidth;
+  
+  canv_game.onmousedown = function (e) {
+    user_x = e.clientX  * scale_f;
+    user_y = e.clientY * scale_f;
+    user_x_ori = user_x;
+    user_y_ori = user_y;
+    
+    if (user_y_ori <= 60 && user_x_ori > 400) {
+      showSubMenu();
+    }
+    
+    user_pressing = true;
+    return false;
+  }
+
+  canv_game.onmouseup = function (e) {
+    user_pressing = false;
+    return false;
+  }
+
+  canv_game.onmousemove = function(e) {
+    user_x = e.layerX * scale_f;
+    user_y = e.layerY * scale_f;
+    return false;
+  }
+
+  canv_game.ontouchstart = function (e) {
+    user_x = e.touches[0].clientX * scale_f;
+    user_y = e.touches[0].clientY * scale_f;
+    user_x_ori = user_x;
+    user_y_ori = user_y;
+    
+    if (user_y_ori <= 60 && user_x_ori > 400) {
+      showSubMenu();
+    }
+    
+    user_pressing = true;
+    return false;
+  }
+
+  canv_game.ontouchend = function (e) {
+    user_pressing = false;
+    return false;
+  }
+
+  canv_game.ontouchmove = function(e) {
+    user_x = e.touches[0].clientX * scale_f;
+    user_y = e.touches[0].clientY * scale_f;
+    return false;
+  }
+}
+
+function showSubMenu() {
+  if (curPage == 'game') {
+    frame.pause = true;
+    document.getElementById( 'bgm' ).pause();
+  }
+  document.getElementById("Submenu").style.width = "100%";
+}
+
+function hideSubMenu() {
+  document.getElementById("Submenu").style.width = "0%";
+  frame.pause = false;
+  if (curPage == 'game') {
+    document.getElementById( 'bgm' ).play();
+    frame.last_animation_time = 0;
+    requestAnimationFrame(tick);
+  }
+}
