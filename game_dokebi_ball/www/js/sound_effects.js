3aa28826d813e3ae45f18a1cc497f9adc032c85c
truemaxdh <truemaxdh@gmail.com>
2020-06-27 18:16:32 +0900

    Update sound_effects.js

--- game_dokebi_ball/www/js/sound_effects.js
+++ game_dokebi_ball/www/js/sound_effects.js
@@ -1,10 +1,10 @@
 // Sound Play
 var sounds = []; // sounds
-sounds[0] = new Audio('sound/coin1.mp3');
+sounds[0] = new Audio('sound/divide.wav');
 sounds[0].volume = 0.7;
-sounds[1] = new Audio('sound/coin2.mp3');
+sounds[1] = new Audio('sound/divide.wav');
 sounds[1].volume = 0.7;
-sounds[2] = new Audio('sound/coin3.mp3');
+sounds[2] = new Audio('sound/divide.wav');
 sounds[2].volume = 0.7;
 	
 function playSound(soundID)
