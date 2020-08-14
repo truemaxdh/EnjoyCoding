fe0a30142bfe1d5220015dd0608daf4c9d884453
truemaxdh <truemaxdh@gmail.com>
2020-08-14 16:14:13 +0900

    Update sound_effects.js

--- interactive_fireworks/www/js/sound_effects.js
+++ interactive_fireworks/www/js/sound_effects.js
@@ -2,12 +2,11 @@
 var sounds = []; // sounds
 for (var i = 0; i < 6; i++) {
   sounds[i] = new Audio('sound/fire' + i + '.mp3');
-  sounds[i].volume = 0.7;
+  sounds[i].volume = 0.4;
 }
 	
 function playSound(soundID)
 {
-  console.log(soundSettings.sound);
   if (soundSettings.sound == "on") {
     try {
       sounds[soundID].currentTime = 0;
