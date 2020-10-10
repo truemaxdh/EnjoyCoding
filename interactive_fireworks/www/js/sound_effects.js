bb85f7f21033343b509f037aad672fc4ad238d03
truemaxdh <truemaxdh@gmail.com>
2020-10-10 18:47:35 +0900

    Update sound_effects.js

--- interactive_fireworks/www/js/sound_effects.js
+++ interactive_fireworks/www/js/sound_effects.js
@@ -7,7 +7,7 @@ for (var i = 0; i < 5; i++) {
     sounds[cnt++].volume = 0.3;
   }
 }
-	
+  
 function playSound(soundID)
 {
   if (soundSettings.sound == "on") {
@@ -20,6 +20,27 @@ function playSound(soundID)
   }
 }
 
+// BGM Play
+var curBgmId = 0;
+var bgms = [new Audio('sound/LightOfNight.mp3'), new Audio('sound/recollection.mp3')]; // BGMs
+for (var i = 0; i < bgms.length; i++) {
+  bgms[i].onended = function() {
+    if (++curBgmId >= bgms.length)
+      curBgmId = 0;
+    bgms[curBgmId].currentTime = 0;
+    bgms[curBgmId].play();
+  }
+}
+
+function playBGM() {
+  bgms[curBgmId].play();
+}
+
+function pauseBGM() {
+  bgms[curBgmId].pause();
+}
+
+// Sound Setting
 var soundSettings = {
   bgm : 'off',
   sound : 'off'
