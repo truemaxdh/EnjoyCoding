900986531cb2df48b7513bcb64f49783248e5f5c
truemaxdh <truemaxdh@gmail.com>
2020-12-25 23:50:45 +0900

    Update render.js

--- game_meteorite_defense/www/js/render.js
+++ game_meteorite_defense/www/js/render.js
@@ -45,6 +45,6 @@ function render() {
   ctx_game.font = '30px Sniglet-ExtraBold';
   ctx_game.fillText('Score : ' + score, 20, 50);
   ctx_game.fillText('Stage : ' + stage, 250,50);
-  ctx_game.fillText('▒〓▤▦', 420,50);
+  ctx_game.fillText('▒', 480,50);
 }
 
