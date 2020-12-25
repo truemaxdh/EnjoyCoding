2e14a104edd91980fafb58e35189210ff2dfc287
truemaxdh <truemaxdh@gmail.com>
2020-12-25 18:37:24 +0900

    Update game.js

--- game_meteorite_defense/www/js/game.js
+++ game_meteorite_defense/www/js/game.js
@@ -5,53 +5,33 @@ var frame = {
     pause : true,
     gameover_flag : false
 }
-/*
-var animation_interval;
-var last_animation_time;
-var pause = true;
-var o_game_over;
-var gameover_flag;
-*/
 
 // concerning score
 var score;
 
 // concerning airplane
 var o_jet;
-var missile_ends = [null, null];
+var missile_0;
 var missile_interval;
 
-// concerning coins
-var coin_ends = [null, null];
-var coin_bullet_ends = [null, null]; 
-var coin_interval;
-var coin_bullet_interval;
+// concerning meteorites
+var met_0;
+var met_interval;
 
 // concerning stage
 var millisec_played;
 var stage;
 
 function _stage_def() {
-    this.max_stage = 10;
-    this.stage_tick = 40000;
+    this.max_stage = 5;
+    this.stage_tick = 1800;
     this.missile_interval = 200;
-    this.coin_interval = 4000;
-    this.bullet_interval = 3000;
-    this.stg1_coin_interval = 4000;
-    this.stg1_bullet_interval = 3000;
-    this.coin_types = [[0], [0], [1], [1], [2], [2], [0, 1, 2], [0, 1, 2], [0, 1, 2], [0, 1, 2]];
-    this.coinBullets = [0, 1, 0, 1, 0, 1, 1, 1, 1, 1];
+    this.met_interval = 4000;
+    this.stg1_met_interval = 4000;
+    this.met_types = [[20], [40], [60], [80], [100]];
 }
 
-var stage_design;/* = {
-    max_stage : 10,
-    stage_tick : 40000,
-    missile_interval : 200,
-    coin_interval : 4000,
-    bullet_interval : 3000,
-    coin_types : [[0], [0], [1], [1], [2], [2], [0, 1, 2], [0, 1, 2], [0, 1, 2], [0, 1, 2]],
-    coinBullets : [0, 1, 0, 1, 0, 1, 1, 1, 1, 1]
-}*/
+var stage_design;
 
 // concerning extra effect
 var effect_flag;
@@ -63,28 +43,16 @@ function game_init() {
     stage_design = new _stage_def();
     missile_interval = stage_design.missile_interval;
     millisec_played = 0;
-    //stage = _stage;
     
-    stage_design.coin_interval -= 400 * (stage - 1);
-    stage_design.bullet_interval -= 300 * (stage - 1);
+    stage_design.met_interval -= 400 * (stage - 1);
             
     o_game_over = null;
     o_jet = new objJet(310, 750);
     
-    missile_ends[0] = new gameobj(0,0), missile_ends[1] = new gameobj(0,0);
-    missile_ends[0].next = missile_ends[1];
-    missile_ends[1].prev = missile_ends[0];
+    missile_0 = new gameobj(0,0);
+    met_0 = new gameobj(0,0);
     
-    coin_ends[0] = new gameobj(0,0), coin_ends[1] = new gameobj(0,0);
-    coin_ends[0].next = coin_ends[1];
-    coin_ends[1].prev = coin_ends[0];
-    
-    coin_bullet_ends[0] = new gameobj(0,0), coin_bullet_ends[1] = new gameobj(0,0);
-    coin_bullet_ends[0].next = coin_bullet_ends[1];
-    coin_bullet_ends[1].prev = coin_bullet_ends[0];
-    
-    coin_interval = 0;
-    coin_bullet_interval = 0;
+    met_interval = 0;
     
     frame.last_animation_time = 0;
     millisec_played = stage_design.stage_tick * (stage - 1) + 1;    
@@ -124,22 +92,23 @@ function gameOver() {
 
 function tick(cur_time) {
     frame.animation_interval = cur_time - (frame.last_animation_time==0 ? cur_time : frame.last_animation_time);
-    frame.last_animation_time = cur_time;
-    millisec_played += frame.animation_interval;
-    
-    if (!effect_flag) {
-        upcoming_obj();
-    }
-    render();
-    if (frame.gameover_flag) {
-        gameOver();
-    } else {
-        proc_user_input();
+    if (animation_interval > 30) {
+        frame.last_animation_time = cur_time;
+        millisec_played += frame.animation_interval;
+
         if (!effect_flag) {
-            collision_check();
+            upcoming_obj();
+        }
+        render();
+        if (frame.gameover_flag) {
+            gameOver();
+        } else {
+            proc_user_input();
+            if (!effect_flag) {
+                collision_check();
+            }
         }
     }
-    
     if (!frame.pause) {
         requestAnimationFrame(tick);
     }
@@ -158,7 +127,7 @@ function proc_user_input() {
         if (missile_interval >= stage_design.missile_interval) {
             missile_interval -= stage_design.missile_interval;
             var o_missile = new objMissile(o_jet.x, o_jet.y);
-            push_to_chain(o_missile, missile_ends);  
+            push_to_chain(o_missile, missile_0);  
         } 
         missile_interval += frame.animation_interval;
     } else {
@@ -170,31 +139,20 @@ function upcoming_obj() {
     // get stage
     if (stage < stage_design.max_stage && millisec_played > (stage_design.stage_tick * stage)) {
         effect_flag = true;
-        coin_ends[0].next = coin_ends[1];
-        coin_ends[1].prev = coin_ends[0];
-        coin_bullet_ends[0].next = coin_bullet_ends[1];
-        coin_bullet_ends[1].prev = coin_bullet_ends[0];
+        met_0[0].next = met_0[1];
+        met_0[1].prev = met_0[0];
         var o_stageClear = new objStageClear(stage);
         push_to_chain(o_stageClear, coin_ends);
-        stage_design.coin_interval -= 400;
-        stage_design.bullet_interval -= 300;
-        coin_interval = 0;
-        coin_bullet_interval = 0;
+        stage_design.met_interval -= 400;
+        met_interval = 0;
         stage++;
     } else {
-        coin_interval += frame.animation_interval;
-        if (stage_design.coin_types[stage-1].length > 0 && coin_interval > stage_design.coin_interval) {
-            var rnd = parseInt(Math.random() * stage_design.coin_types[stage-1].length);
-            var o_coin = new objCoinGray(o_jet.x, o_jet.y, stage_design.coin_types[stage-1][rnd]);
-            push_to_chain(o_coin, coin_ends);  
-            coin_interval -= stage_design.coin_interval;
-        }
-
-        coin_bullet_interval += frame.animation_interval;
-        if (stage_design.coinBullets[stage-1] > 0 && coin_bullet_interval > stage_design.bullet_interval) {
-            var o_coin_bullet = new objCoinBullet(o_jet.x, o_jet.y);
-            push_to_chain(o_coin_bullet, coin_bullet_ends);  
-            coin_bullet_interval -= stage_design.bullet_interval;
+        met_interval += frame.animation_interval;
+        if (stage_design.met_types[stage-1].length > 0 && coin_interval > stage_design.met_interval) {
+            var rnd = parseInt(Math.random() * stage_design.met_types[stage-1].length);
+            var o_met = new objMet(o_jet.x, 0, stage_design.met_types[stage-1][rnd]);
+            push_to_chain(o_met, met_0);  
+            met_interval -= stage_design.met_interval;
         }
     }
 }
@@ -202,59 +160,53 @@ function upcoming_obj() {
 function collision_check() {
     // check collision of missiles and coins
     var o_missile;
-    o_missile = missile_ends[0].next;
+    o_missile = missile_0;
     while(o_missile.next != null) {
-        var o_coin = collision_obj_grp(o_missile, coin_ends);
-        if (o_coin != null) {
-            playSound(--o_coin.durability);
-            remove_from_chain(o_missile, missile_ends);
-            if (o_coin.durability <= 0) {
-                remove_from_chain(o_coin, coin_ends);
-                score += o_coin.coin_num;
-                try {
-                    chkAndUnlockAchievement(score);
-                } catch(err) {}
-            }            
-        }
         o_missile = o_missile.next;
+        var o_met = collision_obj_grp(o_missile, met_0);
+        if (o_met != null) {
+            playSound(1);
+            remove_from_chain(o_missile);
+            remove_from_chain(o_met);
+            score += 20;
+            o_met.size -= 20;
+            if (o_met.size > 0) {
+                push_to_chain(new objMet(o_met.x, o_met.y, o_met.size), met_0);
+                push_to_chain(new objMet(o_met.x, o_met.y, o_met.size), met_0);
+            }            
+            try {
+                chkAndUnlockAchievement(score);
+            } catch(err) {}
+        }
     }
 
-    // check collision of coins and airplane
-    var o_coin = collision_obj_grp(o_jet, coin_ends);
-    if (o_coin != null) {
-        remove_from_chain(o_coin, coin_ends);
-        frame.gameover_flag = true;
-    }
-
-    // check collision of coin bullet and airplane
-    var o_coin_bullet = collision_obj_grp(o_jet, coin_bullet_ends);
-    if (o_coin_bullet != null) {
-        remove_from_chain(o_coin_bullet, coin_bullet_ends);
+    // check collision of mets and airplane
+    var o_met = collision_obj_grp(o_jet, met_0);
+    if (o_met != null) {
+        remove_from_chain(o_met);
         frame.gameover_flag = true;
     }
 }
 
-function push_to_chain(obj, ends) {
-    ends[1].prev.next = obj;
-    obj.prev = ends[1].prev;
-    obj.next = ends[1];
-    ends[1].prev = obj;
+function push_to_chain(obj, obj0) {
+    obj.next = obj0.next;
+    obj0.next = obj;
 }
 
-function remove_from_chain(obj, ends) {
+function remove_from_chain(obj) {
     obj.prev.next = obj.next;
-    obj.next.prev = obj.prev;
 }
 
-function collision_obj_grp(obj, ends) {
+function collision_obj_grp(obj, target0) {
     var ret = null;
     var o_x0 = obj.x + obj.margin_x;
     var o_x1 = (o_x0 - obj.margin_x) + (obj.width - obj.margin_x);
     var o_y0 = obj.y + obj.margin_y;
     var o_y1 = (o_y0 - obj.margin_y) + (obj.height - obj.margin_y);
     
-    var t = ends[0].next;
+    var t = target0;
     while(t.next != null) {
+        t = t.next;
         var t_x0 = t.x + t.margin_x;
         var t_x1 = (t_x0 - t.margin_x) + (t.width - t.margin_x);
         var t_y0 = t.y + t.margin_y;
@@ -263,7 +215,6 @@ function collision_obj_grp(obj, ends) {
             ret = t;
             break;
         }
-        t = t.next;            
     }
 
     return ret;
