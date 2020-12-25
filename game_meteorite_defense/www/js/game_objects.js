609c7105b802885c2638c97d6f29c64fd1125092
truemaxdh <truemaxdh@gmail.com>
2020-12-26 01:50:31 +0900

    Update game_objects.js

--- game_meteorite_defense/www/js/game_objects.js
+++ game_meteorite_defense/www/js/game_objects.js
@@ -109,7 +109,7 @@ function objMet(x, y, size) {
     this.step_y = Math.random() * 7 + 1;
     this.rgbStroke = "rgb(" + (Math.random() * 240 + 16) + "," + (Math.random() * 240 + 16) + "," + (Math.random() * 240 + 16) + ")";
     this.rgbFill = "rgb(" + (Math.random() * 240 + 16) + "," + (Math.random() * 240 + 16) + "," + (Math.random() * 240 + 16) + ")";
-    this.bonusItem = Math.floor(Math.random() * 2);
+    this.bonusItem = (Math.random() < 0.1) ? 1 : 0;
     this.move = function() {
         this.x += this.step_x;
         if (this.x > 540) {
@@ -157,7 +157,7 @@ function objItemProtection(x, y) {
         
         ctx_game.fillStyle = "#cacaca";
         ctx_game.font = '30px Sniglet-ExtraBold';
-        ctx_game.fillText('P', this.x, this.y);
+        ctx_game.fillText('P', this.x, this.y + this.height);
     };
 }
 
