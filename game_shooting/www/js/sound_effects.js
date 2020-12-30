3fc3e280143dc596895252eed2ab7884c638eb7f
truemaxdh <truemaxdh@gmail.com>
2020-12-30 17:41:57 +0900

    Update sound_effects.js

--- game_shooting/www/js/sound_effects.js
+++ game_shooting/www/js/sound_effects.js
@@ -19,8 +19,11 @@ function playSound(soundID)
 
 function playBGM() {
   try {
-    if (soundSettings.bgm == 'on')
-      document.getElementById( 'bgm' ).play();   
+    if (soundSettings.bgm == 'on') {
+      var bgm = document.getElementById( 'bgm' );
+      bgm.volume = 0.5;
+      bgm.play();   
+    }  
   } catch(e) {
     toast(e.message);    
   }
