4b2691e9774b60290dc30de97dac63e45393ef79
truemaxdh <truemaxdh@gmail.com>
2020-12-30 17:42:41 +0900

    Update sound_effects.js

--- game_pentix/www/js/sound_effects.js
+++ game_pentix/www/js/sound_effects.js
@@ -8,8 +8,11 @@ function playSound()
 
 function playBGM() {
   try {
-    if (soundSettings.bgm == 'on')
-      document.getElementById( 'bgm' ).play();   
+    if (soundSettings.bgm == 'on') {
+      var bgm = document.getElementById( 'bgm' );
+      bgm.volume = 0.5;
+      bgm.play();   
+    } 
   } catch(e) {
     toast(e.message);    
   }
