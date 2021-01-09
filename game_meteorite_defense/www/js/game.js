ed5d1e1cda7c0fcfd994095cc5ef417ff3a7421b
truemaxdh <truemaxdh@gmail.com>
2021-01-09 17:01:14 +0900

    Update game.js

--- game_meteorite_defense/www/js/game.js
+++ game_meteorite_defense/www/js/game.js
@@ -37,19 +37,19 @@ var game_design = {
 
 var _stage_design = [
     { stage : 1, met_interval : 1500, met_types : [30, 60] },
-    { stage : 2, met_interval : 1700, met_types : [60] },
+    { stage : 2, met_interval : 1600, met_types : [60] },
     { stage : 3, met_interval : 1500, met_types : [30, 60, 90] },
-    { stage : 4, met_interval : 1700, met_types : [60, 90] },
-    { stage : 5, met_interval : 1900, met_types : [90] },
+    { stage : 4, met_interval : 1600, met_types : [60, 90] },
+    { stage : 5, met_interval : 1700, met_types : [90] },
     { stage : 6, met_interval : 1500, met_types : [30, 60, 90, 120] },
-    { stage : 7, met_interval : 1700, met_types : [60, 90, 120] },
-    { stage : 8, met_interval : 1900, met_types : [90, 120] },
-    { stage : 9, met_interval : 2100, met_types : [120] },
+    { stage : 7, met_interval : 1600, met_types : [60, 90, 120] },
+    { stage : 8, met_interval : 1700, met_types : [90, 120] },
+    { stage : 9, met_interval : 1800, met_types : [120] },
     { stage : 10, met_interval : 1500, met_types : [30, 60, 90, 120, 150] },
-    { stage : 11, met_interval : 1700, met_types : [60, 90, 120, 150] },
-    { stage : 12, met_interval : 1900, met_types : [90, 120, 150] },
-    { stage : 13, met_interval : 2100, met_types : [120, 150] },
-    { stage : 14, met_interval : 2300, met_types : [150] },
+    { stage : 11, met_interval : 1600, met_types : [60, 90, 120, 150] },
+    { stage : 12, met_interval : 1700, met_types : [90, 120, 150] },
+    { stage : 13, met_interval : 1800, met_types : [120, 150] },
+    { stage : 14, met_interval : 1900, met_types : [150] },
 ];
 
 function newStage() {
