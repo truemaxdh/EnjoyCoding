42dc440a5b07d6ee28c45e406b532c136facadcc
truemaxdh <truemaxdh@gmail.com>
2021-01-09 23:01:45 +0900

    Update game.js

--- game_meteorite_defense/www/js/game.js
+++ game_meteorite_defense/www/js/game.js
@@ -85,6 +85,7 @@ function gameOver() {
     if (o_game_over == null) {
         frame.gameover_flag = true;
         o_game_over = new objGameOver();
+        Android.vibrate(100);
     } else if (o_game_over.count_down-- == 0) {
         // clearInterval(objInterval);
         pauseBGM();
