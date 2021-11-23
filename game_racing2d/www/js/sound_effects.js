20552366327579a9ab96463b4f6d09dd08ff93b5
truemaxdh <truemaxdh@gmail.com>
2021-11-23 17:27:29 +0900

    Update sound_effects.js

--- game_racing2d/www/js/sound_effects.js
+++ game_racing2d/www/js/sound_effects.js
@@ -14,3 +14,27 @@ function playSound(soundID)
     sounds[soundID].play();
   } catch(err) {}
 }
+
+
+function playBGM() {
+  try {
+    if (soundSettings.bgm == 'on') {
+      var bgm = document.getElementById( 'bgm' );
+      bgm.volume = 0.5;
+      bgm.play();   
+    } 
+  } catch(e) {
+    toast(e.message);    
+  }
+}
+
+function pauseBGM() {
+  document.getElementById( 'bgm' ).pause();
+}
+
+// Sound Setting
+var soundSettings = {
+  bgm : 'on',
+  sound : 'on'
+}
+
