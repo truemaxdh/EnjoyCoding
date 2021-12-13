cdde2a028ebb558252879e2cbc9ec4245875f7c1
truemaxdh <truemaxdh@gmail.com>
2021-12-13 10:04:31 +0900

    Add files via upload

---
+++ game_split_ball/www/js/sound_effects.js
@@ -0,0 +1,16 @@
+// Sound Play
+var sounds = []; // sounds
+sounds[0] = new Audio('sound/divide.wav');
+sounds[0].volume = 0.7;
+sounds[1] = new Audio('sound/divide.wav');
+sounds[1].volume = 0.7;
+sounds[2] = new Audio('sound/divide.wav');
+sounds[2].volume = 0.7;
+	
+function playSound(soundID)
+{
+  try {
+    sounds[soundID].currentTime = 0;
+    sounds[soundID].play();
+  } catch(err) {}
+}
