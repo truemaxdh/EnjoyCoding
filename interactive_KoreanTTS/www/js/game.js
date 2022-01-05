76654d90ff8cb1b2e2cfaae37c1e475161d45730
truemaxdh <truemaxdh@gmail.com>
2022-01-05 13:09:16 +0900

    Update game.js

--- interactive_KoreanTTS/www/js/game.js
+++ interactive_KoreanTTS/www/js/game.js
@@ -118,8 +118,8 @@ function collision_check() {
     if (oCatched != null) {
       gameObjects.remove_from_chain(oCatched);
       gamePlay.score += 10;
-      TTS.speech(oCatched.hanguel + '아');
-      //playSound(oCatched.hanguel);
+      //TTS.speech(oCatched.hanguel + '아');
+      playSound(oCatched.hanguel);
       try {
         chkAndUnlockAchievement(gamePlay.score);
       } catch(err) {}
