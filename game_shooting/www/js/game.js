0a9b27974b277004be1e368ed7516d71589a868a
truemaxdh <truemaxdh@gmail.com>
2023-02-08 11:00:41 +0900

    Update game.js

--- game_shooting/www/js/game.js
+++ game_shooting/www/js/game.js
@@ -1,35 +1,29 @@
 // concerning game frame
-var frame = {
+let frame = {
     animation_interval : 0,
     last_animation_time : 0,
     pause : true,
     gameover_flag : false
 }
-/*
-var animation_interval;
-var last_animation_time;
-var pause = true;
-var o_game_over;
-var gameover_flag;
-*/
 
 // concerning score
-var score;
+let score;
 
 // concerning airplane
-var o_jet;
-var missile_ends = [null, null];
-var missile_interval;
+let o_jet;
+let missile_ends = [null, null];
+let missile_interval;
 
 // concerning coins
-var coin_ends = [null, null];
-var coin_bullet_ends = [null, null]; 
-var coin_interval;
-var coin_bullet_interval;
+let coin_ends = [null, null];
+let deadCoin_ends = [null, null];
+let coin_bullet_ends = [null, null]; 
+let coin_interval;
+let coin_bullet_interval;
 
 // concerning stage
-var millisec_played;
-var stage;
+let millisec_played;
+let stage;
 
 function _stage_def() {
     this.max_stage = 10;
@@ -43,7 +37,7 @@ function _stage_def() {
     this.coinBullets = [0, 1, 0, 1, 0, 1, 1, 1, 1, 1];
 }
 
-var stage_design;/* = {
+let stage_design;/* = {
     max_stage : 10,
     stage_tick : 40000,
     missile_interval : 200,
@@ -54,7 +48,7 @@ var stage_design;/* = {
 }*/
 
 // concerning extra effect
-var effect_flag;
+let effect_flag;
 
 function game_init() {
     frame.gameover_flag = false;
@@ -147,8 +141,8 @@ function tick(cur_time) {
 
 function proc_user_input() {
     if (user_pressing) {
-        var dx = user_x - user_x_ori;
-        var dy = user_y - user_y_ori;
+        let dx = user_x - user_x_ori;
+        let dy = user_y - user_y_ori;
         user_x_ori = user_x;
         user_y_ori = user_y;
 
@@ -157,7 +151,7 @@ function proc_user_input() {
         
         if (missile_interval >= stage_design.missile_interval) {
             missile_interval -= stage_design.missile_interval;
-            var o_missile = new objMissile(o_jet.x, o_jet.y);
+            let o_missile = new objMissile(o_jet.x, o_jet.y);
             push_to_chain(o_missile, missile_ends);  
         } 
         missile_interval += frame.animation_interval;
@@ -174,7 +168,7 @@ function upcoming_obj() {
         coin_ends[1].prev = coin_ends[0];
         coin_bullet_ends[0].next = coin_bullet_ends[1];
         coin_bullet_ends[1].prev = coin_bullet_ends[0];
-        var o_stageClear = new objStageClear(stage);
+        let o_stageClear = new objStageClear(stage);
         push_to_chain(o_stageClear, coin_ends);
         stage_design.coin_interval -= 400;
         stage_design.bullet_interval -= 300;
@@ -184,15 +178,15 @@ function upcoming_obj() {
     } else {
         coin_interval += frame.animation_interval;
         if (stage_design.coin_types[stage-1].length > 0 && coin_interval > stage_design.coin_interval) {
-            var rnd = parseInt(Math.random() * stage_design.coin_types[stage-1].length);
-            var o_coin = new objCoinGray(o_jet.x, o_jet.y, stage_design.coin_types[stage-1][rnd]);
+            let rnd = parseInt(Math.random() * stage_design.coin_types[stage-1].length);
+            let o_coin = new objCoinGray(o_jet.x, o_jet.y, stage_design.coin_types[stage-1][rnd]);
             push_to_chain(o_coin, coin_ends);  
             coin_interval -= stage_design.coin_interval;
         }
 
         coin_bullet_interval += frame.animation_interval;
         if (stage_design.coinBullets[stage-1] > 0 && coin_bullet_interval > stage_design.bullet_interval) {
-            var o_coin_bullet = new objCoinBullet(o_jet.x, o_jet.y);
+            let o_coin_bullet = new objCoinBullet(o_jet.x, o_jet.y);
             push_to_chain(o_coin_bullet, coin_bullet_ends);  
             coin_bullet_interval -= stage_design.bullet_interval;
         }
@@ -201,16 +195,18 @@ function upcoming_obj() {
 
 function collision_check() {
     // check collision of missiles and coins
-    var o_missile;
+    let o_missile;
     o_missile = missile_ends[0].next;
     while(o_missile.next != null) {
-        var o_coin = collision_obj_grp(o_missile, coin_ends);
+        let o_coin = collision_obj_grp(o_missile, coin_ends);
         if (o_coin != null) {
             playSound(--o_coin.durability);
             remove_from_chain(o_missile, missile_ends);
             if (o_coin.durability <= 0) {
                 remove_from_chain(o_coin, coin_ends);
                 score += o_coin.coin_num;
+                let o_deadCoin = new objDeadCoin(o_coin);
+                push_to_chain(o_deadCoin, deadCoin_ends);  
                 try {
                     chkAndUnlockAchievement(score);
                 } catch(err) {}
@@ -220,14 +216,14 @@ function collision_check() {
     }
 
     // check collision of coins and airplane
-    var o_coin = collision_obj_grp(o_jet, coin_ends);
+    let o_coin = collision_obj_grp(o_jet, coin_ends);
     if (o_coin != null) {
         remove_from_chain(o_coin, coin_ends);
         frame.gameover_flag = true;
     }
 
     // check collision of coin bullet and airplane
-    var o_coin_bullet = collision_obj_grp(o_jet, coin_bullet_ends);
+    let o_coin_bullet = collision_obj_grp(o_jet, coin_bullet_ends);
     if (o_coin_bullet != null) {
         remove_from_chain(o_coin_bullet, coin_bullet_ends);
         frame.gameover_flag = true;
@@ -247,18 +243,18 @@ function remove_from_chain(obj, ends) {
 }
 
 function collision_obj_grp(obj, ends) {
-    var ret = null;
-    var o_x0 = obj.x + obj.margin_x;
-    var o_x1 = (o_x0 - obj.margin_x) + (obj.img.width - obj.margin_x);
-    var o_y0 = obj.y + obj.margin_y;
-    var o_y1 = (o_y0 - obj.margin_y) + (obj.img.height - obj.margin_y);
+    let ret = null;
+    let o_x0 = obj.x + obj.margin_x;
+    let o_x1 = (o_x0 - obj.margin_x) + (obj.img.width - obj.margin_x);
+    let o_y0 = obj.y + obj.margin_y;
+    let o_y1 = (o_y0 - obj.margin_y) + (obj.img.height - obj.margin_y);
     
-    var t = ends[0].next;
+    let t = ends[0].next;
     while(t.next != null) {
-        var t_x0 = t.x + t.margin_x;
-        var t_x1 = (t_x0 - t.margin_x) + (t.img.width - t.margin_x);
-        var t_y0 = t.y + t.margin_y;
-        var t_y1 = (t_y0 - t.margin_y) + (t.img.height - t.margin_y);
+        let t_x0 = t.x + t.margin_x;
+        let t_x1 = (t_x0 - t.margin_x) + (t.img.width - t.margin_x);
+        let t_y0 = t.y + t.margin_y;
+        let t_y1 = (t_y0 - t.margin_y) + (t.img.height - t.margin_y);
         if (o_x0 < t_x1 && o_x1 > t_x0 && o_y0 < t_y1 && o_y1 > t_y0) {
             ret = t;
             break;
