c97a218a76f1087426920aaaa090f33a11fdaadb
truemaxdh <truemaxdh@gmail.com>
2023-02-08 11:26:07 +0900

    Update game_objects.js

--- game_shooting/www/js/game_objects.js
+++ game_shooting/www/js/game_objects.js
@@ -1,35 +1,23 @@
-var img_airplane = new Image();
-var img_airplane_x = new Image();
-var img_missile = new Image();
-var img_coin_golds = [new Image(), new Image(), new Image()];
-var img_coin_grays = [new Image(), new Image(), new Image()];
-var img_coin_bullet = new Image();
+const img_airplane = new Image();
+const img_airplane_x = new Image();
+const img_missile = new Image();
+const img_coin_golds = [new Image(), new Image(), new Image()];
+const img_coin_grays = [new Image(), new Image(), new Image()];
+const img_coin_bullet = new Image();
 
-var img_gameObjs = [
+const img_gameObjs = [
     img_airplane, img_airplane_x, img_missile, 
     img_coin_golds[0], img_coin_golds[1], img_coin_golds[2],
     img_coin_grays[0], img_coin_grays[1], img_coin_grays[2],
     img_coin_bullet
 ];
-var URL_gameObjs = [
+const URL_gameObjs = [
     'img/airplane.png', 'img/airplane_x.png', 'img/missile.png',
     'img/coin_gold_10.png', 'img/coin_gold_50.png', 'img/coin_gold_100.png',
     'img/coin_gray_10.png', 'img/coin_gray_50.png', 'img/coin_gray_100.png',
     //'img/coin_gold_10.png', 'img/coin_gold_50.png', 'img/coin_gold_100.png',
     'img/coin_bullet.png'
 ];
-
-// function game_objects_init() {
-//     var imagesOK = 0; 
-    for (var i=0; i<img_gameObjs.length; i++) {
-        // img_gameObjs[i].onload = function(){ 
-        //     if (++imagesOK>=img_gameObjs.length ) {
-        //         callback();
-        //     }
-        // };
-        img_gameObjs[i].src = URL_gameObjs[i];
-    }    
-//}
 
 function gameobj(x, y) {
     this.x = x;
@@ -80,16 +68,8 @@ function objMissile(x, y) {
     this.step_y = -900;
 }
 
-var type_coinNum = [10, 50, 100];
-//function objCoin(x, y, type) {
-//    gameobj.call(this, x, y);
-//    this.step_x = 0;
-//    this.step_y = 200 + 30 * stage;
-//    this.coin_num = type_coinNum[type];
-//    this.img = img_coin_golds[type];
-//}
-
-var type_durability = [1, 2, 3];
+const type_coinNum = [10, 50, 100];
+const type_durability = [1, 2, 3];
 function objCoinGray(x, y, type) {
     const rnd_x = Math.floor(Math.random() * 540);
     gameobj.call(this, rnd_x, 0);
@@ -122,7 +102,7 @@ function objDeadCoin(baseCoin) {
     this.step_y = baseCoin.step_y;
     this.coin_num = baseCoin.coin_num;
     this.img = baseCoin.img2;
-    this.lifeCnt = 1000;
+    this.count_down = 1000;
     this.render = function(ctx_game) {
         ctx_game.drawImage(this.img, this.x, this.y);
         
@@ -140,7 +120,7 @@ function objDeadCoin(baseCoin) {
 }
 
 function objCoinBullet(x, y) {
-    var rnd_x = Math.floor(Math.random() * 720);
+    let rnd_x = Math.floor(Math.random() * 720);
     gameobj.call(this, rnd_x, 0);
     this.step_y = 200 + 50 * stage;
     this.step_x = (x - rnd_x) * this.step_y / y;
@@ -150,11 +130,11 @@ function objCoinBullet(x, y) {
 function objGameOver() {
     this.count_down = 100;
     this.render = function(ctx_game) {
-        var c_x = ctx_game.canvas.width / 2;
-        var c_y = ctx_game.canvas.height / 2;
+        let c_x = ctx_game.canvas.width / 2;
+        let c_y = ctx_game.canvas.height / 2;
         
         // create radial gradient
-        var grd = ctx_game.createRadialGradient(c_x, c_y, 10, c_x, c_y, 150);
+        let grd = ctx_game.createRadialGradient(c_x, c_y, 10, c_x, c_y, 150);
         // light blue
         grd.addColorStop(0, 'yellow');
         // dark blue
@@ -169,19 +149,20 @@ function objStageClear(stage) {
     chkAndUnlockStage(stage);
     this.count_down = 3000;
     this.render = function(ctx_game) {
-        var c_x = ctx_game.canvas.width / 2;
-        var c_y = ctx_game.canvas.height / 2;
+        let c_x = ctx_game.canvas.width / 2;
+        let c_y = ctx_game.canvas.height / 2;
         
         // create radial gradient
         // Create gradient
-        var grd=ctx_game.createLinearGradient(0, 0, ctx_game.canvas.width,0);
+        let grd=ctx_game.createLinearGradient(0, 0, ctx_game.canvas.width,0);
         grd.addColorStop("0","magenta");
         grd.addColorStop("0.5","blue");
         grd.addColorStop("1.0","red");
         
-        ctx_game.fillStyle = grd;
+        ctx_game.strokeStyle = grd;
+        ctx_game.lineWidth = 3;
         ctx_game.font = '50px Sniglet-ExtraBold';
-        ctx_game.fillText('Stage' + stage + ' Clear!', c_x - 200, c_y - 25);
+        ctx_game.strokeText('Stage' + stage + ' Clear!', c_x - 200, c_y - 25);
         this.count_down -= frame.animation_interval;
         if (this.count_down <= 0) {
             remove_from_chain(this, deadCoin_ends);
